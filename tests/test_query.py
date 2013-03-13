fd42535ce7298794536559ea75dbdac4ae98d0bf
Lincoln de Sousa <lincoln@comum.org>
2013-03-13 15:48:37 -0400

    Updating the basic tests for the query builder
    We are now using a different approach to avoid side-effects
    in our API and to make it more consistent

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,3 +1,4 @@
+import sure
 from datetime import datetime
 from elasticfun import QueryBuilder
 
@@ -17,22 +18,22 @@ def test_query_single_word():
     # Given that I have an instance of the query builder
     qb = QueryBuilder()
 
-    # When I filter by a certain word
-    qb.filter('stuff')
+    # When I filter by a certain word.
+    query = qb('stuff')
 
     # Then I see that the right query was created
-    str(qb).should.equal('(stuff)')
+    str(query).should.equal('stuff')
 
 
 def test_query_single_field():
     # Given that I have an instance of the query builder
     qb = QueryBuilder()
 
-    # When I filter by a certain field
-    qb.filter(brand='blah')
+    # When I filter by a certain field.
+    query = qb(brand='blah')
 
     # Then I see that the right query was created
-    str(qb).should.equal('brand:blah')
+    str(query).should.equal('brand:blah')
 
 
 def test_query_single_boolean_value():
@@ -40,22 +41,22 @@ def test_query_single_boolean_value():
     qb = QueryBuilder()
 
     # When I filter by a boolean value
-    qb.filter(True)
+    query = qb(True)
 
     # Then I see that the right query was created
-    str(qb).should.equal('(True)')
+    str(query).should.equal('True')
 
 
 def test_query_single_datetime_value():
     # Given that I have an instance of the query builder
     qb = QueryBuilder()
 
-    # When I filter by a boolean value
-    qb.filter(datetime(2013, 3, 13, 1, 32))
+    # When I filter by a boolean value.
+    query = qb(datetime(2013, 3, 13, 1, 32))
 
     # Then I see that the right query was created with the date
     # converted to a string in the YYYY-mm-ddTHH:MM:SS format
-    str(qb).should.equal('(2013-03-13T01:32:00)')
+    str(query).should.equal('2013-03-13T01:32:00')
 
 
 def test_query_with_and():
@@ -63,10 +64,10 @@ def test_query_with_and():
     qb = QueryBuilder()
 
     # When I filter by one word after another
-    qb.filter('ice').filter('cream')
+    query = qb('ice') & qb('cream')
 
     # Then I see that the right query was created with the AND operator
-    str(qb).should.equal('((ice) AND (cream))')
+    str(query).should.equal('ice AND cream')
 
 
 def test_query_with_or():
@@ -75,10 +76,10 @@ def test_query_with_or():
 
     # When I filter by one word and then by another word with the OR
     # operator
-    qb.filter('ice').filter_or('cream')
+    query = qb('ice') | ('cream')
 
     # Then I see that the right query was created
-    str(qb).should.equal('((ice) OR (cream))')
+    str(query).should.equal('ice OR cream')
 
 
 def test_query_with_implicit_and():
@@ -86,11 +87,11 @@ def test_query_with_implicit_and():
     qb = QueryBuilder()
 
     # When I filter by two words in the same filter
-    qb.filter('ice cream')
+    query = qb('ice cream')
 
     # Then I see that the right query was created with an AND separating
     # the two words
-    str(qb).should.equal('((ice) AND (cream))')
+    str(query).should.equal('ice AND cream')
 
 
 def test_query_add_boost():
@@ -98,9 +99,8 @@ def test_query_add_boost():
     qb = QueryBuilder()
 
     # When I filter by one word and add a boost to a field
-    qb.filter('stuff')
-    qb.boost('field', 3)
+    query = qb('stuff', boost=('field', 3))
 
     # Then I see that the query contains both, the field and the query
     # boosting
-    str(qb).should.equal('(stuff) field^3')
+    str(query).should.equal('stuff field^3')
