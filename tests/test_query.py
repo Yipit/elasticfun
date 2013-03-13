2ce24b55a57aaee9f068f2d37d1e8fa53fb2fc23
Lincoln de Sousa <lincoln@comum.org>
2013-03-13 19:01:59 -0400

    Improving the Query API tests by enforcing we can perform only one op per instance

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,58 +1,80 @@
 import sure
 from datetime import datetime
-from elasticfun import QueryBuilder
+from elasticfun import Query, ParsingException
 
 
 def test_query_all():
-    # Given that I have an instance of the query builder
-    qb = QueryBuilder()
-
     # When I query for all the objects
-    querystr = str(qb)
+    querystr = str(Query())
 
     # Then I see that the right query was created
     querystr.should.equal('*:*')
 
 
-def test_query_single_word():
-    # Given that I have an instance of the query builder
-    qb = QueryBuilder()
+def test_empty_query():
+    # When I query for nothing
+    querystr = str(Query(_empty=True))
 
+    # Then I see that this query object won't return anything just yet
+    querystr.should.be.empty
+
+
+def test_empty_query_should_blowup_with_fields_or_query():
+    # When I query for nothing
+    Query.when.called_with('blah', _empty=True).should.throw(
+        ParsingException,
+        'You cannot pass fields or words to empty queries')
+
+    Query.when.called_with(field='blah', _empty=True).should.throw(
+        ParsingException,
+        'You cannot pass fields or words to empty queries')
+
+
+def test_mixing_words_and_fields():
+    # When I try to filter by both words and fields in the same object,
+    # Than I see that it actually raised an exception
+    Query.when.called_with('stuff', field='val').should.throw(
+        ParsingException,
+        "You cannot use both words and fields in the same call. "
+        "You should do something like this: qb('stuff') & qb(field='val')")
+
+
+def test_passing_more_than_one_field():
+    # When I try to filter with more than one field in the same object,
+    # Than I see that it actually raised an exception
+    Query.when.called_with(field1='a', field2='b').should.throw(
+        ParsingException,
+        "You cannot use more than one field the same call. "
+        "You should do something like this: qb(field2='b') & qb(field1='a')")
+
+
+def test_query_single_word():
     # When I filter by a certain word.
-    query = qb('stuff')
+    query = Query('stuff')
 
     # Then I see that the right query was created
     str(query).should.equal('stuff')
 
 
 def test_query_single_field():
-    # Given that I have an instance of the query builder
-    qb = QueryBuilder()
-
     # When I filter by a certain field.
-    query = qb(brand='blah')
+    query = Query(brand='blah')
 
     # Then I see that the right query was created
     str(query).should.equal('brand:blah')
 
 
 def test_query_single_boolean_value():
-    # Given that I have an instance of the query builder
-    qb = QueryBuilder()
-
     # When I filter by a boolean value
-    query = qb(True)
+    query = Query(True)
 
     # Then I see that the right query was created
     str(query).should.equal('True')
 
 
 def test_query_single_datetime_value():
-    # Given that I have an instance of the query builder
-    qb = QueryBuilder()
-
     # When I filter by a boolean value.
-    query = qb(datetime(2013, 3, 13, 1, 32))
+    query = Query(datetime(2013, 3, 13, 1, 32))
 
     # Then I see that the right query was created with the date
     # converted to a string in the YYYY-mm-ddTHH:MM:SS format
@@ -60,46 +82,43 @@ def test_query_single_datetime_value():
 
 
 def test_query_with_and():
-    # Given that I have an instance of the query builder
-    qb = QueryBuilder()
-
     # When I filter by one word after another
-    query = qb('ice') & qb('cream')
+    query = Query('ice') & Query('cream')
 
     # Then I see that the right query was created with the AND operator
-    str(query).should.equal('ice AND cream')
+    str(query).should.equal('(ice AND cream)')
 
 
 def test_query_with_or():
-    # Given that I have an instance of the query builder
-    qb = QueryBuilder()
-
     # When I filter by one word and then by another word with the OR
     # operator
-    query = qb('ice') | ('cream')
+    query = Query('ice') | Query('cream')
 
     # Then I see that the right query was created
-    str(query).should.equal('ice OR cream')
+    str(query).should.equal('(ice OR cream)')
 
 
 def test_query_with_implicit_and():
-    # Given that I have an instance of the query builder
-    qb = QueryBuilder()
-
     # When I filter by two words in the same filter
-    query = qb('ice cream')
+    query = Query('ice cream')
 
     # Then I see that the right query was created with an AND separating
     # the two words
-    str(query).should.equal('ice AND cream')
+    str(query).should.equal('(ice AND cream)')
+
+
+def test_query_with_field_and():
+    # When I filter by field with more than one value
+    query = Query(brand='ice cream')
+
+    # Then I see that the value of the field that contaiend two words
+    # was evaluated to an expression
+    str(query).should.equal('brand:(ice AND cream)')
 
 
 def test_query_add_boost():
-    # Given that I have an instance of the query builder
-    qb = QueryBuilder()
-
     # When I filter by one word and add a boost to a field
-    query = qb('stuff', boost=('field', 3))
+    query = Query('stuff', _boost=('field', 3))
 
     # Then I see that the query contains both, the field and the query
     # boosting
