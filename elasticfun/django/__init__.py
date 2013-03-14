ab6c2a7be5eb174c7ed9ca3d20d551168bc43d3b
Lincoln de Sousa <lincoln@comum.org>
2013-03-14 17:26:20 -0400

    Integrating basic features of pyelastic search with django and our query object

---
+++ elasticfun/django/__init__.py
@@ -0,0 +1,37 @@
+import pyelasticsearch
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+
+__all__ = 'elasticfun',
+
+
+class ConfManager(object):
+    @property
+    def connections(self):
+        return settings.ELASTICFUN_CONNECTIONS
+
+    @property
+    def indexes(self):
+        return settings.ELASTICFUN_CONNECTIONS.keys()
+
+
+class ElasticFun(object):
+
+    conf = ConfManager()
+
+    def search(self, query, index='default'):
+        # Looking up the index
+        if index not in self.conf.indexes:
+            raise ImproperlyConfigured((
+                "There's no index called `{}`, the available ones are: {}. "
+                "Check the ELASTICFUN_CONNECTIONS variable in your settings "
+                "file."
+            ).format(index, ', '.join(self.conf.indexes)))
+
+        # Calling the backend search method
+        esurl = self.conf.connections[index]['URL']
+        esinst = pyelasticsearch.ElasticSearch(esurl)
+        return esinst.search(str(query), index=index)
+
+elasticfun = ElasticFun()
