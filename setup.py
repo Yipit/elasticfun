f92728b28e37762caa65d32b5cf2cce266fa1180
Nitya Oberoi <nitya@yipit.com>
2013-03-26 17:08:34 -0400

    Changing version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     setup(
         name="elasticfun",
         license="GPL",
-        version='0.0.2',
+        version='0.1.0',
         description=u'ElasticSearch Query functionality using Django',
         long_description=open('README.md').read(),
         author=u'Lincoln de Sousa, Nitya Oberoi',
