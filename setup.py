1a22af5f0455011a157f185dcaa5c4034cdb629a
Nitya Oberoi <nitya@yipit.com>
2013-04-11 19:23:45 -0400

    Adding parse for requirements file to add in dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,42 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import re
+import os
 from setuptools import setup, find_packages
+
+
+def parse_requirements():
+    """Rudimentary parser for the `requirements.txt` file
+
+    We just want to separate regular packages from links to pass them to the
+    `install_requires` and `dependency_links` params of the `setup()`
+    function properly.
+    """
+    try:
+        requirements = \
+            map(str.strip, local_file('requirements.txt').split('\n'))
+    except IOError:
+        raise RuntimeError("Couldn't find the `requirements.txt' file :(")
+
+    links = []
+    pkgs = []
+    for req in requirements:
+        if not req:
+            continue
+        if 'http:' in req or 'https:' in req:
+            links.append(req)
+            name, version = re.findall("\#egg=([^\-]+)-(.+$)", req)[0]
+            pkgs.append('{0}=={1}'.format(name, version))
+        else:
+            pkgs.append(req)
+
+    return pkgs, links
+
+local_file = lambda f: \
+    open(os.path.join(os.path.dirname(__file__), f)).read()
+
+install_requires, dependency_links = parse_requirements()
 
 
 if __name__ == '__main__':
@@ -27,6 +62,8 @@ if __name__ == '__main__':
         author_email=u'lincoln@yipit.com, nitya@yipit.com',
         url='https://github.com/Yipit/elasticfun',
         packages=find_packages(),
+        install_requires=install_requires,
+        dependency_links=dependency_links,
         classifiers=(
             'Development Status :: 3 - Alpha',
             'Framework :: Django',
